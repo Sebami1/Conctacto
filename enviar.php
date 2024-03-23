ce56f7db6ff3b8ff0e1cd0012ce480bba7afbdad
Sebami1 <perucampeon2018rusia@gmail.com>
2024-03-23 00:56:29 -0500

    cambio de gmail

--- enviar.php
+++ enviar.php
@@ -3,7 +3,6 @@
 ini_set("SMTP", "wolfsotre.shop");
 ini_set("smtp_port", "5847"); 
 
-
 // Datos del formulario
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
@@ -24,6 +23,7 @@ $cuerpoMensaje .= "Email: $email\n";
 $cuerpoMensaje .= "Mensaje:\n$mensaje";
 
 // Cabeceras del correo
+$headers .= "X-Mailer: PHP/" . phpversion();
 $headers = "From: $nombre <$email>\r\n";
 $headers .= "Reply-To: $email\r\n";
 
@@ -38,14 +38,18 @@ $archivoTemporal = $_FILES['archivo']['tmp_name'];
 if (!empty($nombreArchivo) && !empty($archivoTemporal)) {
     $rutaArchivo = $carpetaAdjuntos . $nombreArchivo;
     if (move_uploaded_file($archivoTemporal, $rutaArchivo)) {
+        // Generar un hash aleatorio para delimitar las partes del mensaje
+        $random_hash = md5(date('r', time()));
+        
         // Adjuntar el archivo al correo
         $adjunto = chunk_split(base64_encode(file_get_contents($rutaArchivo)));
-        $headers .= "Content-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\"";
-        $mensaje .= "--PHP-mixed-".$random_hash."";
-        $mensaje .= "Content-Type: application/octet-stream; name=\"".$nombreArchivo."\"";
-        $mensaje .= "Content-Transfer-Encoding: base64";
-        $mensaje .= "Content-Disposition: attachment";
-        $mensaje .= $adjunto;
+        $headers .= "Content-Type: multipart/mixed; boundary=\"PHP-mixed-$random_hash\"\r\n";
+        $mensaje .= "--PHP-mixed-$random_hash\r\n";
+        $mensaje .= "Content-Type: application/octet-stream; name=\"$nombreArchivo\"\r\n";
+        $mensaje .= "Content-Transfer-Encoding: base64\r\n";
+        $mensaje .= "Content-Disposition: attachment; filename=\"$nombreArchivo\"\r\n\r\n";
+        $mensaje .= "$adjunto\r\n";
+        $mensaje .= "--PHP-mixed-$random_hash--\r\n";
     } else {
         echo '<p>Hubo un error al subir el archivo adjunto. Por favor, inténtelo de nuevo más tarde.</p>';
     }
@@ -58,4 +62,3 @@ if (mail($correoCorporativo, $asunto, $mensaje, $headers)) {
     echo '<p>Hubo un error al enviar el mensaje al correo corporativo. Por favor, inténtelo de nuevo más tarde.</p>';
 }
 
-// Envío del correo al correo ingresado en el formulario (si se
