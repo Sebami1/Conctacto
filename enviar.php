cb484021512a5b957f9aee4d16867c3912f15834
Sebami1 <perucampeon2018rusia@gmail.com>
2024-03-23 01:03:41 -0500

    cambio de gmail

--- enviar.php
+++ enviar.php
@@ -1,5 +1,8 @@
 <?php
 
+$random_hash = md5(date('r', time()));
+
+
 ini_set("SMTP", "wolfsotre.shop");
 ini_set("smtp_port", "5847"); 
 
@@ -23,9 +26,9 @@ $cuerpoMensaje .= "Email: $email\n";
 $cuerpoMensaje .= "Mensaje:\n$mensaje";
 
 // Cabeceras del correo
-$headers .= "X-Mailer: PHP/" . phpversion();
 $headers = "From: $nombre <$email>\r\n";
 $headers .= "Reply-To: $email\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
 
 // Carpeta para almacenar archivos adjuntos
 $carpetaAdjuntos = 'archivos_adjuntos/';
@@ -38,18 +41,22 @@ $archivoTemporal = $_FILES['archivo']['tmp_name'];
 if (!empty($nombreArchivo) && !empty($archivoTemporal)) {
     $rutaArchivo = $carpetaAdjuntos . $nombreArchivo;
     if (move_uploaded_file($archivoTemporal, $rutaArchivo)) {
-        // Generar un hash aleatorio para delimitar las partes del mensaje
-        $random_hash = md5(date('r', time()));
-        
         // Adjuntar el archivo al correo
         $adjunto = chunk_split(base64_encode(file_get_contents($rutaArchivo)));
         $headers .= "Content-Type: multipart/mixed; boundary=\"PHP-mixed-$random_hash\"\r\n";
+        $mensaje = "--PHP-mixed-$random_hash\r\n";
+        $mensaje .= "Content-Type: multipart/alternative; boundary=\"PHP-alt-$random_hash\"\r\n\r\n";
+        $mensaje .= "--PHP-alt-$random_hash\r\n";
+        $mensaje .= "Content-Type: text/plain; charset=\"iso-8859-1\"\r\n";
+        $mensaje .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+        $mensaje .= $cuerpoMensaje . "\r\n\r\n";
+        $mensaje .= "--PHP-alt-$random_hash--\r\n\r\n";
         $mensaje .= "--PHP-mixed-$random_hash\r\n";
         $mensaje .= "Content-Type: application/octet-stream; name=\"$nombreArchivo\"\r\n";
         $mensaje .= "Content-Transfer-Encoding: base64\r\n";
-        $mensaje .= "Content-Disposition: attachment; filename=\"$nombreArchivo\"\r\n\r\n";
-        $mensaje .= "$adjunto\r\n";
-        $mensaje .= "--PHP-mixed-$random_hash--\r\n";
+        $mensaje .= "Content-Disposition: attachment\r\n\r\n";
+        $mensaje .= $adjunto . "\r\n\r\n";
+        $mensaje .= "--PHP-mixed-$random_hash--";
     } else {
         echo '<p>Hubo un error al subir el archivo adjunto. Por favor, inténtelo de nuevo más tarde.</p>';
     }
@@ -61,4 +68,14 @@ if (mail($correoCorporativo, $asunto, $mensaje, $headers)) {
 } else {
     echo '<p>Hubo un error al enviar el mensaje al correo corporativo. Por favor, inténtelo de nuevo más tarde.</p>';
 }
+
+// Envío del correo al correo ingresado en el formulario (si se proporcionó)
+if (!empty($correoRemitente)) {
+    if (mail($correoRemitente, $asunto, $cuerpoMensaje, $headers)) {
+        header('Location: index.html');
+    } else {
+        echo '<p>Hubo un error al enviar una copia del mensaje a tu correo electrónico. Por favor, inténtelo de nuevo más tarde.</p>';
+    }
+}
+
 
