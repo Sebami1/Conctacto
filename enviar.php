72d166593fb669ca37230c2ff7aa0a93ae41ccd5
Sebami1 <perucampeon2018rusia@gmail.com>
2024-03-23 01:13:02 -0500

    cambio de gmail

--- enviar.php
+++ enviar.php
@@ -1,7 +1,10 @@
 <?php
 
+$random_hash = md5(date('r', time()));
+
+
 ini_set("SMTP", "wolfsotre.shop");
-ini_set("smtp_port", "5847");
+ini_set("smtp_port", "5847"); 
 
 // Datos del formulario
 $nombre = $_POST['nombre'];
@@ -20,12 +23,12 @@ $asunto = 'Nuevo mensaje desde formulario de contacto';
 // Construir el cuerpo del mensaje
 $cuerpoMensaje = "Nombre: $nombre\n";
 $cuerpoMensaje .= "Email: $email\n";
-$cuerpoMensaje .= "Mensaje:\n$mensaje\n";
+$cuerpoMensaje .= "Mensaje:\n$mensaje";
 
 // Cabeceras del correo
 $headers = "From: $nombre <$email>\r\n";
 $headers .= "Reply-To: $email\r\n";
-$headers .= "Content-Type: multipart/mixed; boundary=\"PHP-mixed-".md5(time())."\"\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
 
 // Carpeta para almacenar archivos adjuntos
 $carpetaAdjuntos = 'archivos_adjuntos/';
@@ -34,38 +37,45 @@ $carpetaAdjuntos = 'archivos_adjuntos/';
 $nombreArchivo = $_FILES['archivo']['name'];
 $archivoTemporal = $_FILES['archivo']['tmp_name'];
 
-// Verificar si se ha proporcionado un archivo adjunto
+// Mover el archivo adjunto a la carpeta de archivos adjuntos
 if (!empty($nombreArchivo) && !empty($archivoTemporal)) {
-    // Mover el archivo adjunto a la carpeta de archivos adjuntos
     $rutaArchivo = $carpetaAdjuntos . $nombreArchivo;
     if (move_uploaded_file($archivoTemporal, $rutaArchivo)) {
-        // Leer el contenido del archivo
-        $contenidoArchivo = file_get_contents($rutaArchivo);
-        // Codificar el archivo adjunto en base64
-        $archivoAdjunto = chunk_split(base64_encode($contenidoArchivo));
-
-        // Definir el límite del mensaje multipart/mixed
-        $boundary = "--PHP-mixed-".md5(time());
-
-        // Construir el cuerpo del mensaje
-        $mensajeAdjunto = "\n\n--$boundary\n";
-        $mensajeAdjunto .= "Content-Type: application/octet-stream; name=\"$nombreArchivo\"\n";
-        $mensajeAdjunto .= "Content-Transfer-Encoding: base64\n";
-        $mensajeAdjunto .= "Content-Disposition: attachment\n\n";
-        $mensajeAdjunto .= "$archivoAdjunto\n";
-        $mensajeAdjunto .= "--$boundary--\n";
-
-        // Agregar el mensaje adjunto al cuerpo del mensaje principal
-        $cuerpoMensaje .= $mensajeAdjunto;
+        // Adjuntar el archivo al correo
+        $adjunto = chunk_split(base64_encode(file_get_contents($rutaArchivo)));
+        $headers .= "Content-Type: multipart/mixed; boundary=\"PHP-mixed-$random_hash\"\r\n";
+        $mensaje = "--PHP-mixed-$random_hash\r\n";
+        $mensaje .= "Content-Type: multipart/alternative; boundary=\"PHP-alt-$random_hash\"\r\n\r\n";
+        $mensaje .= "--PHP-alt-$random_hash\r\n";
+        $mensaje .= "Content-Type: text/plain; charset=\"iso-8859-1\"\r\n";
+        $mensaje .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+        $mensaje .= $cuerpoMensaje . "\r\n\r\n";
+        $mensaje .= "--PHP-alt-$random_hash--\r\n\r\n";
+        $mensaje .= "--PHP-mixed-$random_hash\r\n";
+        $mensaje .= "Content-Type: application/octet-stream; name=\"$nombreArchivo\"\r\n";
+        $mensaje .= "Content-Transfer-Encoding: base64\r\n";
+        $mensaje .= "Content-Disposition: attachment\r\n\r\n";
+        $mensaje .= $adjunto . "\r\n\r\n";
+        $mensaje .= "--PHP-mixed-$random_hash--";
     } else {
         echo '<p>Hubo un error al subir el archivo adjunto. Por favor, inténtelo de nuevo más tarde.</p>';
     }
 }
 
 // Envío del correo al correo corporativo
-if (mail($correoCorporativo, $asunto, $cuerpoMensaje, $headers)) {
+if (mail($correoCorporativo, $asunto, $mensaje, $headers)) {
     header('Location: index.html');
 } else {
     echo '<p>Hubo un error al enviar el mensaje al correo corporativo. Por favor, inténtelo de nuevo más tarde.</p>';
 }
+
+// Envío del correo al correo ingresado en el formulario (si se proporcionó)
+if (!empty($correoRemitente)) {
+    if (mail($correoRemitente, $asunto, $cuerpoMensaje, $headers)) {
+        header('Location: index.html');
+    } else {
+        echo '<p>Hubo un error al enviar una copia del mensaje a tu correo electrónico. Por favor, inténtelo de nuevo más tarde.</p>';
+    }
+}
+
 
